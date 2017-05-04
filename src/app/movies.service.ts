077e33f4c24ac073bebf99edbac81d835dc69c6e
Suat KÖSE <suadev@gmail.com>
2017-05-04 23:24:01 +0300

    Update movies.service.ts

--- src/app/movies.service.ts
+++ src/app/movies.service.ts
@@ -7,9 +7,7 @@ export class MoviesService {
     
     constructor(private http: Http) { }
 
-    public SearchMovie(searchKey: string, searchType: string) {
-
-        
+    public SearchMovie(searchKey: string, searchType: string) {        
         var url: string = 'http://www.omdbapi.com/?{type}=';
         url = url.replace('{type}', searchType);
         return this.http.get(url + searchKey)
